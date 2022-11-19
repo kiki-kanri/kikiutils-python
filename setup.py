4f12c10634535f84e33a3871ea1d55479fec9cab
kiki-kanri <a470666@gmail.com>
2022-11-19 19:31:19 +0800

    Change package name

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='kiki_utils_base',
+    name='kiki_utils',
     classifiers=[
         'License :: Freely Distributable'
     ],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=True,
-    version='1.1.2',
+    version='1.0.0',
     description='Utils functions without cv2 and pillow',
     author='kiki-kanri',
     author_email='a470666@gmail.com',
@@ -23,6 +23,7 @@ setup(
         'pyopenssl',
         'python-magic;platform_system=="Linux"',
         'python-magic-bin;platform_system=="Windows"',
+        'quart',
         'requests',
         'validator'
     ],
