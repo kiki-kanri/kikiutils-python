a393c25acbc0a889cd6d84cd18f007b76cd9dc0e
kiki-kanri <a470666@gmail.com>
2023-07-19 15:11:40 +0800

    Edit LICENSE, README and script.

--- build_upload.sh
+++ build_upload.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-python3.11 setup.py sdist bdist_wheel
-python3.11 -m twine upload dist/*
-rm -rf build
-rm -rf dist
-rm -rf kiki_utils.egg-info
+python3.11 setup.py sdist bdist_wheel &&
+	python3.11 -m twine upload dist/* &&
+	rm -rf build dist kiki_utils.egg-info
